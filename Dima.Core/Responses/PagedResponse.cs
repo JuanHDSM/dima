dd9c7337894294748f4758f0223d08822d45e52b
JuanHoly <juanholydesouza@gmail.com>
2024-08-29 00:42:11 -0400

    feat: create Responses

---
+++ Dima.Core/Responses/PagedResponse.cs
@@ -0,0 +1,29 @@
+using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses
+{
+    public class PagedResponse<TData> : Response<TData> where TData : class
+    {
+
+        [JsonConstructor]
+        public PagedResponse(TData? data, int totalCount, int currentPage = 1, int pageSize = Configuration.DefaultPageSize)
+            :base(data)
+        {
+            Data = data;
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+        }
+
+        public PagedResponse(TData data, int code, string? message = null)
+            :base(data, code, message)
+        {
+            
+        }
+
+        public int CurrentPage { get; set; } = Configuration.DefaultPageNumber;
+        public int TotalPage  => (int)Math.Ceiling((double) TotalCount / PageSize);
+        public int PageSize { get; set; } = Configuration.DefaultPageSize;
+        public int TotalCount { get; set; }
+    }
+}

c64694f4fdf745a83fef141e556e582aeb72641b
Juan Holy <juanholydesouza@gmail.com>
2024-09-03 04:40:19 -0400

    fix: deploy workflow

--- Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93595cd8c0bdbdfe8834c564fdc81936d48ea06d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6612efcc8d6439ada60363cf0b7fd33297a9e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

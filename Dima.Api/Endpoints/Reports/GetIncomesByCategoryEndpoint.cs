7f590c2c21f2fcd66c17eb0dc94b99367e8c2712
JuanHoly <juanholydesouza@gmail.com>
2024-09-19 06:24:45 -0400

    feat: Creating endpoints to reports

---
+++ Dima.Api/Endpoints/Reports/GetIncomesByCategoryEndpoint.cs
@@ -0,0 +1,37 @@
+using System.Security.Claims;
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Reports
+{
+    public class GetIncomesByCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/incomes", HandleAsync)
+                .WithName("Incomes: Get Expenses By Category")
+                .WithSummary("Obtem entrada por categoria")
+                .WithDescription("Obtem entrada por categoria")
+                .WithOrder(4)
+                .Produces<Response<List<IncomesByCategory>?>>();
+
+        private static async Task<IResult> HandleAsync(
+            ClaimsPrincipal user,
+            IReportHandler handler
+        )
+        {
+            var request = new GetIncomesByCategoryRequest
+            {
+                UserId = user.Identity?.Name ?? string.Empty
+            };
+            
+            var result = await handler.GetIncomesByCategoryReportAsync(request);
+
+            return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+        }
+    }
+}

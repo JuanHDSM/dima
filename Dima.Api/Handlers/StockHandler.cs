28828a8dc7a9c2a99e445974a79e4b6c6eee42ce
JuanHoly <juanholydesouza@gmail.com>
2024-09-24 06:26:35 -0400

    feat: Creating handlers to stocks on api

---
+++ Dima.Api/Handlers/StockHandler.cs
@@ -0,0 +1,25 @@
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Stocks;
+using Dima.Core.Responses;
+using stocks.Responses;
+
+namespace Dima.Api.Handlers
+{
+    public class StockHandler(IHttpClientFactory httpClientFactory) : IStockHandler
+    {
+        private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.StocksHttpClientName);
+        public async Task<Response<StockResponse>> GetAllStocksAsync(GetAllStocksRequest request)
+        {
+            try
+            {
+                var result = await _client.GetFromJsonAsync<StockResponse>($"/api/quote/list?limit=1619&page=1&token={Configuration.TokenStockService}");
+                return new Response<StockResponse>(result);
+            }
+            catch (Exception e)
+            {
+                return new Response<StockResponse>(null, 400, e.Data.ToString());
+            }
+        }
+    }
+}

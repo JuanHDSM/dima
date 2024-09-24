9ea804ac5d97eb7c3fe4ccccfbf995ae51414003
JuanHoly <juanholydesouza@gmail.com>
2024-09-24 06:30:35 -0400

    feat: Configuring cors to stocks

--- Dima.Core/Configuration.cs
+++ Dima.Core/Configuration.cs
@@ -5,10 +5,11 @@ namespace Dima.Core
         public const int DefaultSatusCode = 200;
         public const int DefaultPageNumber = 1;
         public const int DefaultPageSize = 25;
-
         public static string ConnectionString { get; set; } = string.Empty;
-
         public static string FrontendUrl { get; set; } = string.Empty;
         public static string BackendUrl { get; set; } = string.Empty;
+        public static string StockApiUrl { get; set; } = string.Empty;
+        public static string StocksHttpClientName { get; set; } = "stocks";
+        public static string TokenStockService { get; set; } = "94op5kYVVLwmMPt1qZMTd8";
     }
 }

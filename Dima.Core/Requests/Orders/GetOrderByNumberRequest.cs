44aebf4d5f567fb0f7f3074f916530ae5f22134d
JuanHoly <juanholydesouza@gmail.com>
2024-12-12 00:31:25 -0400

    feat: adding request to Order, Product and Vouche. creating interfaces handler to this models Order, Product, Voucher

---
+++ Dima.Core/Requests/Orders/GetOrderByNumberRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Dima.Core.Requests.Orders;
+
+public class GetOrderByNumberRequest : BaseRequest
+{
+    public string Number { get; set; } = string.Empty;
+}
---
+++ Dima.Core/Requests/Orders/PayOrderRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Dima.Core.Requests.Orders;
+
+public class PayOrderRequest : BaseRequest
+{
+    public long Id { get; set; }
+    public string ExternalReference { get; set; } = string.Empty;
+}

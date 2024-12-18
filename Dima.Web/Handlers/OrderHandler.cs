6d14ca7159d37151dd91ec6e6daba85d560fbaff
JuanHoly <juanholydesouza@gmail.com>
2024-12-18 01:43:20 -0400

    feat: implementing handler of the Voucher, Product and Order

--- Dima.Web/Handlers/OrderHandler.cs
+++ Dima.Web/Handlers/OrderHandler.cs
@@ -1,6 +1,47 @@
-﻿namespace Dima.Web.Handlers;
+﻿using System.Net.Http.Json;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
 
-public class OrderHandler
+namespace Dima.Web.Handlers;
+
+public class OrderHandler(IHttpClientFactory httpClientFactory) : IOrderHandler
 {
-    
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+    public async Task<Response<Order?>> CancelAsync(CancelOrderRequest request)
+    {
+        var result = await _client.PostAsJsonAsync($"api/v1/orders/{request.Id}/cancel", request);
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>() 
+            ?? new Response<Order?>(null, 400, "Não foi possível cancelar este pedido");
+    }
+
+    public async Task<Response<Order?>> CreateAsync(CreateOrderRequest request)
+    {
+        var result = await _client.PostAsJsonAsync("api/v1/orders", request);
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>() 
+               ?? new Response<Order?>(null, 400, "Não foi possível cancelar este pedido");
+    }
+
+    public async Task<Response<Order?>> PayAsync(PayOrderRequest request)
+    {
+        var result = await _client.PostAsJsonAsync($"api/v1/orders/{request.Id}/pay", request);
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>() 
+               ?? new Response<Order?>(null, 400, "Não foi possível pagar este pedido");
+    }
+
+    public async Task<Response<Order?>> RefundAsync(RefundOrderRequest request)
+    {
+        var result = await _client.PostAsJsonAsync($"api/v1/orders/{request.Id}/refund", request);
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>() 
+               ?? new Response<Order?>(null, 400, "Não foi possível reembolsar este pedido");
+    }
+
+    public async Task<PagedResponse<List<Order>?>> GetAllAsync(GetAllOrderRequest request)
+        => await _client.GetFromJsonAsync<PagedResponse<List<Order>?>>("api/v1/orders")
+           ?? new PagedResponse<List<Order>?>(null, 400, "Não foi possóver obter os pedidos");
+
+    public async Task<Response<Order?>> GetByNumberAsync(GetOrderByNumberRequest request)
+        => await _client.GetFromJsonAsync<Response<Order?>>($"api/v1/orders/{request.Number}")
+           ?? new Response<Order?>(null, 400, "Não foi possível obter o produto");
 }
